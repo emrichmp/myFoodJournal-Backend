955eb7091bb712857adc41c3f2b0dadd56a6b94b
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-12 18:44:26 -0400

    added rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :goals, only: [:index, :create]
+      resources :journal, only: [:index, :update, :create]
+      resources :meals
+      resources :foods
+    end
+  end
 end
