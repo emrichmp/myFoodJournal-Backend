e078d384e15f1a4d4ad5fe41586ab4691d710fb1
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-12 18:17:21 -0400

    attempted something, reverted changes

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
     #has_many :meals, through: :mealfoods
+    #has_and_belongs_to_many :meals
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,5 @@
 class Meal < ApplicationRecord
     belongs_to :journal
     #has_many :foods, through: :mealfoods
+    #has_and_belongs_to_many :foods
 end
