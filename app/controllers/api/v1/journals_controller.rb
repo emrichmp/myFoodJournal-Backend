e30e3ea5bdde38c6560c3885ca19930acd24014f
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-13 11:54:46 -0400

    added show action, still need to finish

--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::JournalsController < ApplicationController
         #render json: journals
         render json: JournalSerializer.new(journals)
     end
+
+    # def show
+
+    # end
 end
