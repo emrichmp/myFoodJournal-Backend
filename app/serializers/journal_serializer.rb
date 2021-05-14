5eb0f116006c3f15c411e43d2bccfd250d7f685f
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-14 17:10:00 -0400

    added has_many_through relationship to journal and meal and foods, updated serializer to show food.

--- app/serializers/journal_serializer.rb
+++ app/serializers/journal_serializer.rb
@@ -1,8 +1,4 @@
 class JournalSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :date, :meals#, :foods
-
-  # def foods
-  #   ActiveModel::SerializableResource.new(object.foods,  each_serializer: FoodSerializer)
-  # end
+  attributes :date, :meals, :foods
 end
