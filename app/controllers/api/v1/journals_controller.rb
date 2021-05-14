0226d06694a3ff3a97339baa3607f9e0425030a4
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-14 17:07:43 -0400

    revereted some attempted changes

--- app/controllers/api/v1/journals_controller.rb
+++ app/controllers/api/v1/journals_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::JournalsController < ApplicationController
     def index
         journals = Journal.all
-        #render json: journals
         render json: JournalSerializer.new(journals)
     end
 
--- app/serializers/journal_serializer.rb
+++ app/serializers/journal_serializer.rb
@@ -1,4 +1,8 @@
 class JournalSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :date, :meals
+  attributes :date, :meals#, :foods
+
+  # def foods
+  #   ActiveModel::SerializableResource.new(object.foods,  each_serializer: FoodSerializer)
+  # end
 end
