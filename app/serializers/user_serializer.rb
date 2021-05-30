09b8ab73dfd4a456db1184a059964cf2233206a2
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-29 22:10:58 -0400

    reverted changes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :goal, :journals#, :meals
-  # def journals
-  #   self.object.journals.map do |journal|
-  #     {meals: journal.meals}
-  #   end
-  # end 
+  attributes :goal, :journals
 end
