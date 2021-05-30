156c24865438e0319dcb6ed6ff3b125ac3f8bd23
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-05-29 22:07:04 -0400

    tried to add meals

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,9 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :goal, :journals
-
+  attributes :goal, :journals#, :meals
+  # def journals
+  #   self.object.journals.map do |journal|
+  #     {meals: journal.meals}
+  #   end
+  # end 
 end
