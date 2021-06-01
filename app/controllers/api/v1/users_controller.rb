ef4a74528eb351fdcb2333440faff36e582fb362
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-01 14:03:04 -0400

    added create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,5 +8,14 @@ class Api::V1::UsersController < ApplicationController
         user = User.find_by_id(params[:id])
         render json: UserSerializer.new(user)
     end
+
+    def create
+        user = User.new(user_params)
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
     
 end
