d413ae7e59f67d32831d223c9a5ce5c11afdeaaa
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-01 14:03:52 -0400

    added :weight to strong params

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :weight)
     end
     
 end
