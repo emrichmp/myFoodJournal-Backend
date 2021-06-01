f286155e867ded1ef5527defbc0dc12cd002cfd6
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-01 16:27:09 -0400

    rendering json in controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,6 +12,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.new(user_params)
         user.save
+        render json: UserSerializer.new(user)
     end
 
     private
