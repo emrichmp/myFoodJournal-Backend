1fdf41bb2c7022b63cc3a738918cec20d804ca1c
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-02 12:26:41 -0400

    had create action return user id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
-            render json: UserSerializer.new(user)
+            render json: user.id
         else
             render json: ("problem").to_json
         end
