910bee39c1f8e00ed4f63f7a2f9816c35092b77c
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-02 16:45:22 -0400

    added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password(validations: false)
     has_one :goal
     has_many :journals
+    validates :weight, numericality: { only_integer: true }
+    validates :email, :password, :weight, presence: true
 end
