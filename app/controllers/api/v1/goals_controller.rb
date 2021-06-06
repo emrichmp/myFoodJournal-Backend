774df6dbcd53b4edc7603304121423d785da7b77
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-06-06 17:46:40 -0400

    added create action in goal

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -13,7 +13,12 @@ class Api::V1::GoalsController < ApplicationController
     end
 
     def create
-        #goal = Goal.new(goal_params)
+        goal = Goal.new(goal_params)
+        if goal.save
+            render json: GoalSerializer.new(goal)
+        else
+            render json: "problem".to_json
+        end
     end
 
     def update
@@ -24,6 +29,6 @@ class Api::V1::GoalsController < ApplicationController
     private
 
   def goal_params
-    params.require(:goal).permit(:calories, :protein, :fats, :carbs, :goalweight, :id)
+    params.require(:goal).permit(:calories, :protein, :fats, :carbs, :goalweight, :user_id)
   end
 end
