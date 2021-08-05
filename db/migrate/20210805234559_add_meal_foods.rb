1afae592e133efec8b9c5427fb742ae86663d988
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-08-05 16:47:49 -0700

    added fake table

---
+++ db/migrate/20210805234559_add_meal_foods.rb
@@ -0,0 +1,7 @@
+class AddMealFoods < ActiveRecord::Migration[6.1]
+  def change
+    create_table :mealfoods do |t|
+      #nothing
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_162919) do
+ActiveRecord::Schema.define(version: 2021_08_05_234559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 2021_06_07_162919) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "calories_consumed"
+  end
+
+  create_table "mealfoods", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
