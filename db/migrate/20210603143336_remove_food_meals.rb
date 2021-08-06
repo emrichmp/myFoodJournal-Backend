3009a966d6f65ed7312f4c4f6771d5af5c03d8a0
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-08-05 17:04:06 -0700

    lets see

--- db/migrate/20210603143336_remove_food_meals.rb
+++
@@ -1,5 +0,0 @@
-# class RemoveFoodMeals < ActiveRecord::Migration[6.1]
-#   def change
-#     drop_table :food_meals
-#   end
-# end
--- db/migrate/20210607162834_remove_meals.rb
+++
@@ -1,5 +0,0 @@
-class RemoveMeals < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :meals
-  end
-end
